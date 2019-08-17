65fa5301e3ad76185f0148c670a1910e3b2a3b16
boretti <mathieu.boretti@gmail.com>
2019-08-17 16:12:03 +0200

    Add DoubleFunction sample and javadoc

--- src/main/java/ch/powerunit/extensions/exceptions/DoubleFunctionWithException.java
+++ src/main/java/ch/powerunit/extensions/exceptions/DoubleFunctionWithException.java
@@ -19,6 +19,7 @@
  */
 package ch.powerunit.extensions.exceptions;
 
+import static ch.powerunit.extensions.exceptions.Constants.EXCEPTIONMAPPER_CANT_BE_NULL;
 import static ch.powerunit.extensions.exceptions.Constants.FUNCTION_CANT_BE_NULL;
 import static java.util.Objects.requireNonNull;
 
@@ -29,10 +30,10 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * Represents a function that accepts one argument, may throw exception and
- * produces a result.
+ * Represents a function that accepts a double-valued argument and produces a
+ * result. This is the {@code double}-consuming primitive specialization for
+ * {@link FunctionWithException}.
  *
- * @author borettim
  * @see DoubleFunction
  * @param <R>
  *            the type of the result of the function
@@ -41,23 +42,23 @@ import java.util.function.Supplier;
  */
 @FunctionalInterface
 public interface DoubleFunctionWithException<R, E extends Exception>
-		extends ObjectReturnExceptionHandlerSupport<DoubleFunction<R>, Function<Double, Optional<R>>, R> {
+		extends ObjectReturnExceptionHandlerSupport<DoubleFunction<R>, DoubleFunction<Optional<R>>, R> {
 
 	/**
 	 * Applies this function to the given argument.
 	 *
-	 * @param t
+	 * @param value
 	 *            the function argument
 	 * @return the function result
 	 * @throws E
 	 *             any exception
 	 * @see DoubleFunction#apply(double)
 	 */
-	R apply(double t) throws E;
+	R apply(double value) throws E;
 
 	/**
 	 * Converts this {@code DoubleFunctionWithException} to a {@code DoubleFunction}
-	 * that convert exception to {@code RuntimeException}.
+	 * that wraps to {@code RuntimeException}.
 	 *
 	 * @return the unchecked function
 	 * @see #unchecked(DoubleFunctionWithException)
@@ -76,7 +77,7 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 	 * @see #lifted(DoubleFunctionWithException)
 	 */
 	@Override
-	default Function<Double, Optional<R>> lift() {
+	default DoubleFunction<Optional<R>> lift() {
 		return t -> ObjectReturnExceptionHandlerSupport.unchecked(() -> Optional.ofNullable(apply(t)),
 				notThrowingHandler());
 	}
@@ -95,7 +96,7 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 
 	/**
 	 * Convert this {@code DoubleFunctionWithException} to a lifted
-	 * {@code DoubleFunction} return {@code CompletionStage} as return value.
+	 * {@code DoubleFunction} returning {@code CompletionStage} as return value.
 	 *
 	 * @return the lifted function
 	 * @see #staged(DoubleFunctionWithException)
@@ -110,7 +111,7 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 	 * @param exceptionBuilder
 	 *            the supplier to create the exception
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the exception
 	 * @return a function that always throw exception
@@ -123,17 +124,19 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 
 	/**
 	 * Converts a {@code FunctionWithException} to a {@code DoubleFunction} that
-	 * convert exception to {@code RuntimeException}.
+	 * wraps to {@code RuntimeException}.
 	 *
 	 * @param function
 	 *            to be unchecked
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the potential exception
 	 * @return the unchecked exception
 	 * @see #uncheck()
 	 * @see #unchecked(DoubleFunctionWithException, Function)
+	 * @throws NullPointerException
+	 *             if function is null
 	 */
 	static <R, E extends Exception> DoubleFunction<R> unchecked(DoubleFunctionWithException<R, E> function) {
 		return requireNonNull(function, FUNCTION_CANT_BE_NULL).uncheck();
@@ -141,30 +144,32 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 
 	/**
 	 * Converts a {@code DoubleFunctionWithException} to a {@code DoubleFunction}
-	 * that convert exception to {@code RuntimeException} by using the provided
-	 * mapping function.
+	 * that wraps to {@code RuntimeException} by using the provided mapping
+	 * function.
 	 *
 	 * @param function
 	 *            the be unchecked
 	 * @param exceptionMapper
 	 *            a function to convert the exception to the runtime exception.
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the potential exception
-	 * @return the unchecked exception
+	 * @return the unchecked function
 	 * @see #uncheck()
 	 * @see #unchecked(DoubleFunctionWithException)
+	 * @throws NullPointerException
+	 *             if function or exceptionMapper is null
 	 */
 	static <R, E extends Exception> DoubleFunction<R> unchecked(DoubleFunctionWithException<R, E> function,
 			Function<Exception, RuntimeException> exceptionMapper) {
 		requireNonNull(function, FUNCTION_CANT_BE_NULL);
-		requireNonNull(exceptionMapper, "exceptionMapper can't be null");
+		requireNonNull(exceptionMapper, EXCEPTIONMAPPER_CANT_BE_NULL);
 		return new DoubleFunctionWithException<R, E>() {
 
 			@Override
-			public R apply(double t) throws E {
-				return function.apply(t);
+			public R apply(double value) throws E {
+				return function.apply(value);
 			}
 
 			@Override
@@ -182,13 +187,15 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 	 * @param function
 	 *            to be lifted
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the potential exception
 	 * @return the lifted function
 	 * @see #lift()
+	 * @throws NullPointerException
+	 *             if function is null
 	 */
-	static <R, E extends Exception> Function<Double, Optional<R>> lifted(DoubleFunctionWithException<R, E> function) {
+	static <R, E extends Exception> DoubleFunction<Optional<R>> lifted(DoubleFunctionWithException<R, E> function) {
 		return requireNonNull(function, FUNCTION_CANT_BE_NULL).lift();
 	}
 
@@ -199,11 +206,13 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 	 * @param function
 	 *            to be lifted
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the potential exception
 	 * @return the lifted function
 	 * @see #ignore()
+	 * @throws NullPointerException
+	 *             if function is null
 	 */
 	static <R, E extends Exception> DoubleFunction<R> ignored(DoubleFunctionWithException<R, E> function) {
 		return requireNonNull(function, FUNCTION_CANT_BE_NULL).ignore();
@@ -216,11 +225,13 @@ public interface DoubleFunctionWithException<R, E extends Exception>
 	 * @param function
 	 *            to be lifted
 	 * @param <R>
-	 *            the type of the output object to the function
+	 *            the type of the result of the function
 	 * @param <E>
 	 *            the type of the potential exception
 	 * @return the lifted function
 	 * @see #stage()
+	 * @throws NullPointerException
+	 *             if function is null
 	 */
 	static <R, E extends Exception> DoubleFunction<CompletionStage<R>> staged(
 			DoubleFunctionWithException<R, E> function) {
