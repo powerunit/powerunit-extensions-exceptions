75a1c212363a0760e42278608498398fe6ab0741
boretti <mathieu.boretti@gmail.com>
2019-08-18 10:04:31 +0200

    Test failure

--- src/test/java/ch/powerunit/extensions/exceptions/samples/BiConsumerSamplesTest.java
+++ src/test/java/ch/powerunit/extensions/exceptions/samples/BiConsumerSamplesTest.java
@@ -38,6 +38,8 @@ public class BiConsumerSamplesTest implements TestSuite {
 
 	@Test
 	public void sample1() {
+		
+		fail("test for link with code");
 
 		Handler<String> handler = new Handler<>();
 
